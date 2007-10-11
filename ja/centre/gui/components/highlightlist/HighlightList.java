649709c555f42837300925d0aa33702d67db5218
cumeo89 <cumeo89@25be58be-a838-0410-a211-e3037a7e05e3>
2007-10-11 18:59:29 +0000

    scroll

--- ja/centre/gui/components/highlightlist/HighlightList.java
+++ ja/centre/gui/components/highlightlist/HighlightList.java
@@ -28,140 +28,173 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.Vector;
 
-/**
- * Test comment
- */
 public class HighlightList extends JList {
-    private int lastHighLightedIndex = -1;
+	private int highlightedIndex = -1;
 
-    public HighlightList( ListModel dataModel ) {
-        super( dataModel );
-        initialize();
-    }
+	public HighlightList(ListModel dataModel) {
+		super(dataModel);
+		initialize();
+	}
 
-    public HighlightList( final Object[] listData ) {
-        super( listData );
-        initialize();
-    }
+	public HighlightList(final Object[] listData) {
+		super(listData);
+		initialize();
+	}
 
-    public HighlightList( final Vector<?> listData ) {
-        super( listData );
-        initialize();
-    }
+	public HighlightList(final Vector<?> listData) {
+		super(listData);
+		initialize();
+	}
 
-    public HighlightList() {
-        initialize();
-    }
+	public HighlightList() {
+		initialize();
+	}
 
-    private void initialize() {
-        setPrototypeCellValue( "Index 1234567890qypjQYPJ" );
+	private void initialize() {
+		setPrototypeCellValue("Index 1234567890qypjQYPJ");
+		
+		final MouseAdapter mouseAdapter =  new MouseAdapter() {
 
-        addMouseListener( new MouseAdapter() {
-            public void mouseExited( MouseEvent e ) {
-                removeLastHighlighted();
-            }
-        } );
+			@Override
+			public void mouseExited(MouseEvent e) {
+				highlight(-1);
+			}
 
-        addMouseMotionListener( new MouseMotionListener() {
-            public void mouseDragged( MouseEvent e ) {
-                highlight( e );
-            }
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				highlight(e.getPoint());
+			}
+				
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				highlight(e.getPoint());
+			}
 
-            public void mouseMoved( MouseEvent e ) {
-                highlight( e );
-            }
-        } );
-/*
-        addMouseWheelListener( new MouseWheelListener() {
-            public void mouseWheelMoved( MouseWheelEvent e ) {
-                highlight( e );
-            }
-        } );
-*/
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				highlight(e.getPoint());
+			}
 
-        setCellRenderer( new GenericListCellRenderer() );
-    }
+		};
+		
+		addMouseListener(mouseAdapter);
+		
+		addMouseMotionListener(mouseAdapter);
+		
+		setCellRenderer(new GenericListCellRenderer());
+		/*
+		 * this code doesn't work properly because JList would take up mouse
+		 * wheel events and JScrollPane has no oppotunities to be scrollable.
+		 */
 
-    private void removeLastHighlighted() {
-        int backHighlightIndex = lastHighLightedIndex;
-        lastHighLightedIndex = -1;
+		/*
+		 * addMouseWheelListener( new MouseWheelListener() { public void
+		 * mouseWheelMoved( MouseWheelEvent e ) { highlight( e ); } } );
+		 */
+	}
 
-        if ( backHighlightIndex >= 0 ) {
-            enqueCellRepaint( backHighlightIndex );
-        }
-    }
+	private JScrollPane scrollPane;
 
-    private void highlight( MouseEvent e ) {
-        int index = e.getY() / getFixedCellHeight();
+	public JScrollPane getScrollPane() {
+		return scrollPane;
+	}
 
-        if ( lastHighLightedIndex == index ) {
-            return;
-        }
+	/**
+	 * Register containing scroll pane with the list so that
+	 * it can know when user move the mouse wheel on scroll pane.
+	 * @param newScrollPane - containing scroll pane
+	 */
+	public void registerScrollPane(JScrollPane newScrollPane) {
+		final MouseWheelListener mouseWheelListener = new MouseWheelListener() {
+			@Override
+			public void mouseWheelMoved(MouseWheelEvent e) {
+				assert scrollPane != null; // it certainly can not be null
+				highlight( new Point(
+						e.getX() + scrollPane.getHorizontalScrollBar().getValue(), 
+						e.getY() + scrollPane.getVerticalScrollBar().getValue() ) );
+			}
+		};
 
-        if ( index < getModel().getSize() ) {
-            // enque redraw of old and new highlighted cells
-            if ( lastHighLightedIndex >= 0 ) {
-                enqueCellRepaint( lastHighLightedIndex );
-            }
+		if (scrollPane != newScrollPane) {
+			if (scrollPane != null) {
+				scrollPane.removeMouseWheelListener(mouseWheelListener);
+			}
+			if (newScrollPane != null) {
+				newScrollPane.addMouseWheelListener(mouseWheelListener);
+			}
+			this.scrollPane = newScrollPane;
+		}
+	}
 
-            enqueCellRepaint( index );
+	private void highlight(Point p) {
+		highlight(p.y / getFixedCellHeight());
+	}
 
-            lastHighLightedIndex = index;
+	private void highlight(int newIndex) {
+		if (this.highlightedIndex != newIndex) {
+			int oldIndex = this.highlightedIndex;
+			this.highlightedIndex = newIndex;
 
-            setCursor( Cursor.getPredefinedCursor( Cursor.HAND_CURSOR ) );
-        } else {
-            if ( lastHighLightedIndex >= 0 ) {
-                enqueCellRepaint( lastHighLightedIndex );
+			if (oldIndex >= 0 && oldIndex < getModel().getSize()) {
+				enqueCellRepaint(oldIndex);
+			}
 
-                lastHighLightedIndex = -1;
-            }
+			if (newIndex >= 0 && newIndex < getModel().getSize()) {
+				enqueCellRepaint(newIndex);
+				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			} else {
+				setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+			}
+		}
+	}
 
-            setCursor( Cursor.getPredefinedCursor( Cursor.DEFAULT_CURSOR ) );
-        }
-    }
+	private void enqueCellRepaint(int cellIndex) {
+		assert cellIndex >= 0 && cellIndex < getModel().getSize();
+		repaint(0, cellIndex * getFixedCellHeight(), getWidth(), getFixedCellHeight());
+	}
 
-    private void enqueCellRepaint( int cellIndex ) {
-        if ( cellIndex < 0 ) {
-            throw new IndexOutOfBoundsException( "Can't render call with index " + cellIndex );
-        }
+	private class GenericListCellRenderer extends DefaultListCellRenderer {
+		public JLabel getListCellRendererComponent(JList list, Object value,
+				int index, boolean isSelected, boolean cellHasFocus) {
+			JLabel label = (JLabel) super.getListCellRendererComponent(list,
+					value, index, isSelected, cellHasFocus);
 
-        repaint( 0, cellIndex * getFixedCellHeight(),
-                getWidth(), getFixedCellHeight() );
-    }
+			if (highlightedIndex == index) {
+				Color color = ColorUtil.middle(label.getBackground(), UIManager
+						.getColor("MenuItem.selectionBackground"));
 
-    private class GenericListCellRenderer extends DefaultListCellRenderer {
-        public JLabel getListCellRendererComponent( JList list, Object value, int index, boolean isSelected, boolean cellHasFocus ) {
-            JLabel label = (JLabel) super.getListCellRendererComponent( list, value, index, isSelected, cellHasFocus );
+				// label.setForeground( Color.WHITE );
+				label.setBackground(color); // TODO cache color results?;
+				// label.setText( "<html><u>" + label.getText() + "</u></html>"
+				// );
+				// label.setBorder( BorderFactory.createLineBorder( Color.GRAY
+				// ));
+			}
 
-            if ( lastHighLightedIndex == index ) {
-                Color color = ColorUtil.middle(
-                        label.getBackground(),
-                        UIManager.getColor( "MenuItem.selectionBackground" ) );
+			return label;
+		}
+	}
 
-                //label.setForeground( Color.WHITE );
-                label.setBackground( color ); // TODO cache color results?;
-                //label.setText( "<html><u>" + label.getText() + "</u></html>" );
-                //label.setBorder( BorderFactory.createLineBorder( Color.GRAY ));
-            }
+	public static void main(String[] args) {
+		//JaLingoLookAndFeel.install(14);
 
-            return label;
-        }
-    }
+		HighlightList list = new HighlightList(new Integer[] { 1, 2, 3, 4, 5,
+				6, 7, 8, 9 });
 
-    public static void main( String[] args ) {
-        JaLingoLookAndFeel.install( 14 );
+		JFrame frame = new JFrame("HighlightList Demo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JScrollPane scrollPane = Components.scrollVertical(list);
+		list.registerScrollPane(scrollPane);
+		frame.setContentPane(scrollPane);
+		frame.setSize(150, 100);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 
-        HighlightList list = new HighlightList( new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 } );
-
-        JFrame frame = new JFrame( "HighlightList Demo" );
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.setContentPane( Components.scrollVertical( list ) );
-        frame.setSize( 150, 300 );
-        frame.setLocationRelativeTo( null );
-        frame.setVisible( true );
-
-    }
+	}
 }
